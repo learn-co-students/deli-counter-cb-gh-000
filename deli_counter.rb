38ad9dea151f141ecd7b55096775fb73d0b468f8
Aurangzaib Danial Liaqat Khan <aurangzaib.danial@gmail.com>
2017-05-24 07:07:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,11 +18,3 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
 end
 
-def now_serving(katz_deli)
-  if (katz_deli == [])
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli.first}."
-    katz_deli.shift
-  end
-end
