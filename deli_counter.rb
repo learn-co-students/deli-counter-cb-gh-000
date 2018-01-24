732a175da4929072cf441f73620fa10ef0203e61
Othman Mashaab <red.fantastic@hotmail.com>
2018-01-24 08:50:44 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line array
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: "
+    array.each_with_index do |name,index|
+      print "#{index+1}. #{name}"
+    end
+  end
+end
