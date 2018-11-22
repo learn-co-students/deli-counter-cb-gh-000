334240df46c66dbababb4e5bd676e20c6102b954
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-22 16:22:32 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def line(queue)
+  if queue.size == 0
+    puts "The line is currently empty."
+  else
+    print "The line is currently:"
+    queue.each{|customer| print " #{queue.index(customer)}. #{customer}"}
+end
+
+def take_a_number(queue, customer)
+  queue.push(customer)
+  puts "Welcome, #{customer}. You are number #{customer.size} in line."
+end
+
+def now_serving(queue)
+  if queue.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{queue.first}."
+    queue.shift
+  end
+end
