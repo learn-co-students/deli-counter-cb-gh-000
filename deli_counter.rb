8e8e96b8345588556f7c83c5576a08e88f5ad04c
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-09 21:28:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def line(array)
+  if (array.size == 0)
+    puts "The line is currently empty."
+  else
+    i = 0
+    line = "The line is currently:"
+    while (i < array.size)
+      line << " #{i+1}. #{array[i]}"
+      i += 1
+    end
+    puts line
+  end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+end
+
+def now_serving(array)
+  if (array.size == 0)
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.first}."
+    array.shift
+  end
+end
