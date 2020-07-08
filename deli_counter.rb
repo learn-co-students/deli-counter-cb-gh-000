e3f29ce0a932fd2351aca8452be25e4ad3971cdc
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-08 20:29:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,20 @@
 # Write your code here.
 def take_a_number(array, name)
+  array.push(name)
   puts "Welcome, #{name}. You are number #{array.size} in line."
 end
 
 def line(array)
   if array.size==0
-    puts "the line is empty"
+    puts "The line is currently empty."
   else
-  print "The line is currently:"
+  ch="The line is currently:"
   number=1
   array.each do |name|
-    print " #{number}. #{name}"
+    ch<<" #{number}. #{name}"
     number+=1
   end
+  puts ch
 end
 end
 
@@ -20,7 +22,7 @@ def now_serving(array)
   if array.size==0
     puts "There is nobody waiting to be served!"
   else
-    puts array[0]
+    puts "Currently serving #{array[0]}."
     array.shift
   end
 end
